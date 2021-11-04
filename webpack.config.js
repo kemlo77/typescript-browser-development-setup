f8090f3379f618718c52a70217aef5b721649438
Fredrik Kemling <kemling@gmail.com>
2021-11-04 21:35:30 +0100

    webpack-dev-server and html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/main.ts',
@@ -21,4 +22,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true
     },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/template.html' })
+    ]
 };
